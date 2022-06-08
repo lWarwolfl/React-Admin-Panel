913aed1d5991df06852ee47c2412db59fc05249a
NIBBA <m.sinakheiry2@gmail.com>
2022-06-08 17:08:01 +0430

    v1.0

---
+++ src/components/topBar/TopBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './topBar.css';
+
+export default function TopBar() {
+  return (
+		<div className="topBar">
+			<div className="topBarWrapper">
+				<div className="topLeft">left</div>
+				<div className="topRight">right</div>
+			</div>
+		</div>
+	);
+}
