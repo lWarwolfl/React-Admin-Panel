337474e86427957da2e5c02b87b6dbac0f0e2c9d
NIBBA <m.sinakheiry2@gmail.com>
2022-06-08 17:39:35 +0430

    v1.1

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './topBar.css';
+import React from "react";
+import "./topBar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 export default function TopBar() {
-  return (
+	return (
 		<div className="topBar">
 			<div className="topBarWrapper">
-				<div className="topLeft">left</div>
-				<div className="topRight">right</div>
+				<div className="topLeft">
+					<span className="logo boldText">LOGO</span>
+				</div>
+				<div className="topRight">
+                    <div className="topBarIconContainer">
+                        <NotificationsNoneIcon />
+                        <span className="iconBadge mediumText">2</span>
+                    </div>
+				</div>
 			</div>
 		</div>
 	);
